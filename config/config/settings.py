061664cabc07034d540696d3fcfd31e649e0eae3
danial sadri <danielsadri01@gmail.com>
2024-04-06 18:16:31 +0330

    update

--- config/config/settings.py
+++ config/config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 LOCAL_APPS = [
     'notifications.apps.NotificationsConfig',
