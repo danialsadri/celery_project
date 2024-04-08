ebb5d28ecfcdd88a3b9bdc55cd9423e3a05c8448
danial sadri <danielsadri01@gmail.com>
2024-04-08 12:06:32 +0330

    update

--- celery_worker/celery_base.py
+++ celery_worker/celery_base.py
@@ -2,3 +2,5 @@ from celery import Celery
 
 app = Celery('tasks')
 app.config_from_object('celery_config')
+app.conf.imports = ('notifications.tasks',)
+app.autodiscover_tasks()
--- config/config/celery.py
+++ config/config/celery.py
@@ -4,4 +4,8 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.task_routes = {
+    'notifications.tasks.send_discount_emails': {'queue': 'queue1'},
+    'notifications.tasks.process_data_for_machine_learning': {'queue': 'queue2'},
+}
 app.autodiscover_tasks()
