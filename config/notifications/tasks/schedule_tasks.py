8280b932cb07615b641d2b131f21e3143ad6ea55
danial sadri <danielsadri01@gmail.com>
2024-05-08 22:14:32 +0330

    update

--- config/notifications/tasks/schedule_tasks.py
+++ config/notifications/tasks/schedule_tasks.py
@@ -1,15 +1,17 @@
-from datetime import timedelta
 from celery import shared_task
 from config.celery import app
+from celery.schedules import crontab
+from datetime import timedelta
+
 
 app.conf.beat_schedule = {
     'task_1': {
         'task': 'notifications.tasks.schedule_tasks.task_1',
-        'schedule': timedelta(seconds=5),
+        'schedule': crontab(minute='*/1'),
     },
     'task_2': {
         'task': 'notifications.tasks.schedule_tasks.task_2',
-        'schedule': timedelta(seconds=10),
+        'schedule': timedelta(seconds=30),
     },
 }
 
